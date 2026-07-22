351abd8158b62804ba327727313002d458ffb351
card-shop <kartikrawat6266@gmail.com>
2026-07-22 16:32:22 +0530

    Aura

--- config.py
+++ config.py
@@ -3,13 +3,9 @@ TOKEN = "8771905727:AAEJq2QVVSe8OxZOqLkatVK1wGysO9UyzCQ"
 OWNER_ID = 1987818347
 OWNER_USERNAME = "FathersOfCreater"
 
-# 🔥 TUMHARI MONGODB URI - FIXED (Special characters handle)
+# 🔥 MONGODB URI (Optional - JSON fallback hai)
 MONGO_URI = "mongodb+srv://bgmipower9999_db_user:bgmi123@cluster0.qgej2dh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-# Channel IDs
-FORWARD_CHANNEL_ID = -4564006861
-CHANNEL_ID = -4564006861
-
 # Attack Settings
-MAX_THREADS = 5000
+MAX_THREADS = 1500
 MAX_TIME = 600
